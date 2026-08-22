067bc93eafaabd32e4d049c75517bfad4a84df38
mirkamilovic <pubgim39@gmail.com>
2026-08-22 15:02:46 +0500

    Create bash run_bot.sh

---
+++ bash run_bot.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Hunter Mafia botini ishga tushirish skripti.
+# Bu fayl botingizni token va ID bilan ishga tushiradi.
+
+# Token va ID ni o'zgaruvchi sifatida o'rnatamiz
+export HUNTER_MAFIA_TOKEN="8826679011:AAGWEPGy-AKO9Nqp1KjLMurDWMNu2ApA5Fo"
+export HUNTER_MAFIA_OWNER_ID="5588583777"
+
+# Python kodini ishga tushiramiz
+python3 hunter_mafia_v6_hardened.py
